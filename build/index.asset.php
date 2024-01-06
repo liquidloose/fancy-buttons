b07772a8e572e86dbddde31605f6123f1704657e
liquidloose <ronaldlussier83@gmail.com>
2024-01-06 07:02:40 +0000

    All set! QA and testing is aheadgit add -u!

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '507225987bade659cbf8');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'fd8fd5c08fb9e9f68609');
--- fancy-buttons.php
+++ fancy-buttons.php
@@ -26,7 +26,14 @@ function create_block_fancy_buttons_block_init() {
 }
 add_action('init', 'create_block_fancy_buttons_block_init');
 
-function enqueue_remix_icon_css() {
-	wp_enqueue_style('remix-icon-css', 'https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css', array(), null);
+function enqueue_plugin_files() {
+	//wp_enqueue_style('remix-icon-css', 'https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css', array(), null);
+	wp_enqueue_script('fancy-buttons', plugin_dir_url(__FILE__) . 'js/fancy-buttons.js');
 }
-add_action('wp_enqueue_scripts', 'enqueue_remix_icon_css');
+add_action('wp_enqueue_scripts', 'enqueue_plugin_files');
+
+function admin_enqueue_plugin_files() {
+	//wp_enqueue_style('remix-icon-css', 'https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css', array(), null);
+	wp_enqueue_script('fancy-buttons', plugin_dir_url(__FILE__) . 'js/fancy-buttons.js');
+}
+add_action('admin_enqueue_scripts', 'admin_enqueue_plugin_files');
