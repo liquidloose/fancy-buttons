d9d4817fc1a96ba75d6b43d4f9268051726f2782
liquidloose <ronaldlussier83@gmail.com>
2024-01-22 00:38:24 +0000

    run build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9f67d4d29c78866fcf9e');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f1b36532060a077b8734');
